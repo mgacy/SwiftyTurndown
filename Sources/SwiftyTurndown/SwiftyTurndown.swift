5b45ea2e725bbe35d784f2e3d6493c490207206e
Matt Gacy <matt@mgacy.com>
2024-09-07 16:23:42 -0700

    Basic entry point implementation

--- Sources/SwiftyTurndown/SwiftyTurndown.swift
+++ Sources/SwiftyTurndown/SwiftyTurndown.swift
@@ -1,6 +1,33 @@
-public struct SwiftyTurndown {
-    public private(set) var text = "Hello, World!"
+//
+//  SwiftyTurndown.swift
+//  SwiftyTurndown
+//
+//  Created by Mathew Gacy on 7/8/23.
+//
 
-    public init() {
+import Foundation
+
+public struct SwiftyTurndown {
+    let configuration: TurndownConfiguration
+
+    public init(configuration: TurndownConfiguration) {
+        self.configuration = configuration
+    }
+
+    public func markdownify(html: String) throws -> String {
+        let escapedHTML = html
+            .split(separator: "\n")
+            .map { $0.trimmingCharacters(in: .whitespaces) }
+            .joined()
+            .replacingOccurrences(of: #"""#, with: #"\""#)
+
+        let quoted = """
+        "\(escapedHTML)"
+        """
+
+        var arguments = [html]
+        arguments.append(contentsOf: configuration.arguments)
+
+        return ""
     }
 }
