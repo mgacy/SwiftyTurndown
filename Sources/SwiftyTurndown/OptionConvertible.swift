52a795647cda81726c43732007676fbd8da90967
Matt Gacy <matt@mgacy.com>
2025-11-23 14:26:51 -0700

    Documentation

--- Sources/SwiftyTurndown/OptionConvertible.swift
+++ Sources/SwiftyTurndown/OptionConvertible.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
+/// A class of types that can be converted to command line options.
 protocol OptionConvertible {
+    /// The name of the command line option.
     static var optionName: String { get }
+
+    /// The value of the command line option.
     var optionValue: String { get }
 }
 
 extension OptionConvertible {
+    /// The command line option as an array of strings.
     var option: [String] {
         ["--\(Self.optionName)", optionValue]
     }
